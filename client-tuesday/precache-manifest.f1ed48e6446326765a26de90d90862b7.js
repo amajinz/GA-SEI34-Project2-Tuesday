89ebb19a0e41db82896ebcdf58c65be04753bc23
Jeffrey Quan <jeffrey.quan@hotmail.com>
2019-11-30 22:43:32 +1100

    Updates

---
+++ client-tuesday/precache-manifest.f1ed48e6446326765a26de90d90862b7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09783a300bc827fe4ddf8e9994194265",
+    "url": "/tuesday/index.html"
+  },
+  {
+    "revision": "9c6368938de523a751d2",
+    "url": "/tuesday/static/css/2.c946ee25.chunk.css"
+  },
+  {
+    "revision": "e5f95c60ae5e11aaae07",
+    "url": "/tuesday/static/css/main.19b37a54.chunk.css"
+  },
+  {
+    "revision": "9c6368938de523a751d2",
+    "url": "/tuesday/static/js/2.65659653.chunk.js"
+  },
+  {
+    "revision": "e5f95c60ae5e11aaae07",
+    "url": "/tuesday/static/js/main.0d9e1f49.chunk.js"
+  },
+  {
+    "revision": "5828b78fa5ccd0b9d030",
+    "url": "/tuesday/static/js/runtime-main.b1318d6c.js"
+  },
+  {
+    "revision": "abb7a539efbd7039565c112fbab8d20f",
+    "url": "/tuesday/static/media/logo.abb7a539.svg"
+  },
+  {
+    "revision": "b73cbbf90d58a0d57684ebe2aa9a507f",
+    "url": "/tuesday/static/media/logo2.b73cbbf9.svg"
+  }
+]);
